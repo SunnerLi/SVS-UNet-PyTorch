99401a609342d1793b4d593ac9acda7549236954
SunnerLi <a6214123@gmail.com>
2019-06-06 09:58:05 +0800

    first add loader containing

--- train.py
+++ train.py
@@ -1,15 +1,58 @@
 from model import UNet
+from matplotlib import pyplot as plt
 import torch.utils.data as Data
+import numpy as np
 import argparse
+import librosa
 import torch
 import os
 
-class SpectrogramLoader(Data.DataLoader):
-    pass
+class SpectrogramDataset(Data.Dataset):
+    def __init__(self, path):
+        self.path = path
+        self.files = os.listdir(os.path.join(path, 'mixture'))
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        """
+            root --+-- mixture
+                   |
+                   +-- drums
+                   |
+                   +-- bass
+                   |
+                   +-- rest
+                   |
+                   +-- vocal
+        """
+        # import stempeg
+
+        # wav, sr = librosa.load(self.files[idx], mono=False)
+
+        # audio, rate = stempeg.read_stems(
+        #                 filename=self.files[idx],
+        #                 stem_id=4
+        #             )
+
+        # import sounddevice as sd
+        # from librosa.display import waveplot
+        # # waveplot(wav[1])
+        # # waveplot(wav[0])
+        # # plt.tight_layout()
+        # # plt.show()
+        # sd.play(audio, rate, blocking=True)
+        # print(audio.shape)
+
+        mix = np.load(os.path.join(self.path, 'mixture', self.files[idx]))
+        voc = np.load(os.path.join(self.path, 'vocal', self.files[idx]))
+        return mix, voc
 
 
 """
     Dataset: https://sigsep.github.io/datasets/musdb.html
+    Ref: https://github.com/Jeongseungwoo/Singing-Voice-Separation
 """
 
 # 1. Parse the parameters
@@ -22,7 +65,10 @@ parser.add_argument('--epoch', type = int, default = 1)
 args = parser.parse_args()
 
 # 2. Create the data loader
-loader = SpectrogramLoader()
+loader = Data.DataLoader(
+    dataset = SpectrogramDataset('MUS-STEMS-SAMPLE/train'),
+    batch_size=1, num_workers=0
+)
 
 # 3. Load the pre-trained model
 model = UNet()
